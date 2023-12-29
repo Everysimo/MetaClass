09ff86cba84978808a3b5fd67d86b872b56d6326
Michele <m.pesce13@studenti.unisa.it>
2023-12-29 17:04:04 +0100

    fix(backend):
    update UtenteInMeeting entity

---
+++ backend/src/main/java/com/commigo/metaclass/MetaClass/entity/UtenteInMeeting.java
@@ -0,0 +1,31 @@
+package com.commigo.metaclass.MetaClass.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class UtenteInMeeting {
+
+    @Id
+    @NotNull(message = "L'utente non può essere nullo")
+    @ManyToOne()
+    @JoinColumn(name = "id_utente")
+    private Utente utente;
+
+    @Id
+    @NotNull(message = "il meeting non può essere nullo")
+    @ManyToOne()
+    @JoinColumn(name = "id_meeting")
+    private Meeting meeting;
+
+    @NotNull(message = "IsOnline non può essere nullo")
+    private boolean IsOnline;
+}
