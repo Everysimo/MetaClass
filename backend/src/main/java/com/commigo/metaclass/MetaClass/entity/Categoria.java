44750437ee1267da8a894394fc825f04f3188376
carmi <c.detta@studenti.unisa.it>
2023-12-30 11:01:42 +0100

    feat(backend):
    mdoifcia

--- backend/src/main/java/com/commigo/metaclass/MetaClass/entity/Categoria.java
+++ backend/src/main/java/com/commigo/metaclass/MetaClass/entity/Categoria.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Categoria {
 
+    
     public static final int MAX_NAME_LENGTH = 254;
     public static final int MIN_NAME_LENGTH = 1;
 
