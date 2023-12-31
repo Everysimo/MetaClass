7bcf1773902b17667738c95064ebfdafece178dc
fgatto13 <f.gatto13@studenti.unisa.it>
2023-12-31 14:45:23 +0100

    refactor(frontend):
    refactored some components, updated the login page and stylesheet

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -3,13 +3,14 @@ import '../css/MyApp.css';
 import '../css/index.css';
 import {MyHeader} from '../components/Header/Header';
 import {MyFooter} from "../components/Footer/Footer";
+import {SectionGenerator} from "../components/functions";
 export const Login = () =>{
     return(
         <body>
             <header>
                 <MyHeader />
             </header>
-
+            <SectionGenerator class={"sec"} ID={"sec1"}/>
             <footer>
                 <MyFooter />
             </footer>
