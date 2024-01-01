972ceadea9727c3541805f56fca23e5056e297a0
LittleShyStar02 <vincenzocutolo10@gmail.com>
2024-01-01 17:51:36 +0100

    feat(backend): Room creation
    [+] Added method to create a room

--- backend/src/main/java/com/commigo/metaclass/MetaClass/gestionestanza/service/GestioneStanzaServiceImpl.java
+++ backend/src/main/java/com/commigo/metaclass/MetaClass/gestionestanza/service/GestioneStanzaServiceImpl.java
@@ -3,9 +3,11 @@ package com.commigo.metaclass.MetaClass.gestionestanza.service;
 import com.commigo.metaclass.MetaClass.entity.StatoPartecipazione;
 import com.commigo.metaclass.MetaClass.entity.*;
 import com.commigo.metaclass.MetaClass.gestionestanza.controller.RispostaRichiestaAccessoStanza;
-import com.commigo.metaclass.MetaClass.gestionestanza.repository.*;
+import com.commigo.metaclass.MetaClass.gestionestanza.repository.RuoloRepository;
+import com.commigo.metaclass.MetaClass.gestionestanza.repository.StanzaRepository;
+import com.commigo.metaclass.MetaClass.gestionestanza.repository.StatoPartecipazioneRepository;
 import com.commigo.metaclass.MetaClass.gestioneutenza.repository.UtenteRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.commigo.metaclass.MetaClass.utility.Validator;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,41 +19,10 @@ import org.springframework.stereotype.Service;
 @Transactional    //ogni operazione è una transazione
 public class GestioneStanzaServiceImpl implements GestioneStanzaService {
 
-    private final StanzaRepository stanzaRepository;
     private final StatoPartecipazioneRepository statoPartecipazioneRepository;
-    private final UtenteRepository utenteRepository;
     private final RuoloRepository ruoloRepository;
-
-    @Override
-    public ResponseEntity<RispostaRichiestaAccessoStanza> richiestaAccessoStanza(String codiceStanza, String id_utente) {
-        try {
-            Stanza stanza = stanzaRepository.findStanzaByCodice(codiceStanza);
-
-            Utente u = utenteRepository.findFirstByMetaId(id_utente);
-            StatoPartecipazione statoPartecipazione = statoPartecipazioneRepository.findStatoPartecipazioneByUtenteAndStanza(u, stanza);
-
-            if (statoPartecipazione == null) {
-                statoPartecipazione = new StatoPartecipazione(stanza, u, getRuolo(Ruolo.PARTECIPANTE), true, false, u.getNome());
-                return ResponseEntity.ok(new RispostaRichiestaAccessoStanza(true, "La stanza è privata, sei in attesa di entrare"));
-
-            } else if (statoPartecipazione.isBannato()) {
-                return ResponseEntity.ok(new RispostaRichiestaAccessoStanza(false, "Sei stato bannato da questa stanza, non puoi entrare"));
-
-            } else if (statoPartecipazione.isInAttesa()) {
-                return ResponseEntity.ok(new RispostaRichiestaAccessoStanza(true, "Sei già in attesa di entrare in questa stanza"));
-
-            } else {
-                return ResponseEntity.ok(new RispostaRichiestaAccessoStanza(true, "Sei già all'interno di questa stanza"));
-            }
-
-
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new RispostaRichiestaAccessoStanza(false, "Errore durante la richiesta: " + e.getMessage()));
-        }
-
-    }
+    private final StanzaRepository stanzaRepository;
+    private final UtenteRepository utenteRepository;
 
     @Override
     public ResponseEntity<RispostaRichiestaAccessoStanza> accessoStanza(String codiceStanza, String id_utente) {
@@ -89,6 +60,19 @@ public class GestioneStanzaServiceImpl implements GestioneStanzaService {
         }
     }
 
+    @Override
+    public Stanza creaStanza(String nome, String Codice_Stanza, String Descrizione, boolean Tipo_Accesso, int MAX_Posti)
+    {
+        Stanza stanza = null;
+        boolean isValid = Validator.isValid(nome) && Validator.isValid(Codice_Stanza) && Validator.isValid(Descrizione)
+                && Validator.isValid(MAX_Posti);
+        if(!isValid) return stanza;
+
+        stanza = new Stanza(nome, Codice_Stanza, Descrizione, Tipo_Accesso, MAX_Posti);
+        stanzaRepository.save(stanza);
+        return stanza;
+    }
+
     public Ruolo getRuolo(String nome){
 
         Ruolo ruolo = ruoloRepository.findByNome(nome);
@@ -99,5 +83,36 @@ public class GestioneStanzaServiceImpl implements GestioneStanzaService {
             ruoloRepository.save(ruolo);
         }
         return ruolo;
+    }
+
+    @Override
+    public ResponseEntity<RispostaRichiestaAccessoStanza> richiestaAccessoStanza(String codiceStanza, String id_utente) {
+        try {
+            Stanza stanza = stanzaRepository.findStanzaByCodice(codiceStanza);
+
+            Utente u = utenteRepository.findFirstByMetaId(id_utente);
+            StatoPartecipazione statoPartecipazione = statoPartecipazioneRepository.findStatoPartecipazioneByUtenteAndStanza(u, stanza);
+
+            if (statoPartecipazione == null) {
+                statoPartecipazione = new StatoPartecipazione(stanza, u, getRuolo(Ruolo.PARTECIPANTE), true, false, u.getNome());
+                return ResponseEntity.ok(new RispostaRichiestaAccessoStanza(true, "La stanza è privata, sei in attesa di entrare"));
+
+            } else if (statoPartecipazione.isBannato()) {
+                return ResponseEntity.ok(new RispostaRichiestaAccessoStanza(false, "Sei stato bannato da questa stanza, non puoi entrare"));
+
+            } else if (statoPartecipazione.isInAttesa()) {
+                return ResponseEntity.ok(new RispostaRichiestaAccessoStanza(true, "Sei già in attesa di entrare in questa stanza"));
+
+            } else {
+                return ResponseEntity.ok(new RispostaRichiestaAccessoStanza(true, "Sei già all'interno di questa stanza"));
+            }
+
+
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new RispostaRichiestaAccessoStanza(false, "Errore durante la richiesta: " + e.getMessage()));
+        }
+
     }
 }
