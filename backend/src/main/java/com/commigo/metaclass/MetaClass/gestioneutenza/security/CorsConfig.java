3a7936f76ba1fd12bef6e089e9a9d6a1a53858ac
Michele <m.pesce13@studenti.unisa.it>
2024-01-02 19:22:26 +0100

    fix(frontend, backend):
    fixed connection backend and frontend

---
+++ backend/src/main/java/com/commigo/metaclass/MetaClass/gestioneutenza/security/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.commigo.metaclass.MetaClass.gestioneutenza.security;
+
+/*import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+@Configuration
+@EnableWebFluxSecurity
+public class CorsConfig{
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                // ...
+                .cors(cors -> cors.disable());
+        return http.build();
+    }
+}
+*/
