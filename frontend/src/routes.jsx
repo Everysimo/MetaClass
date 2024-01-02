781d9d5749b9b865271430324d5e6bec55fa465a
carmi <c.detta@studenti.unisa.it>
2024-01-02 18:13:02 +0100

    feat(frontend):
    aggiunta routes per la createstanza

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,14 +1,16 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {Login} from "./pages/login";
-import {LoggedIn, loggedIn} from "./pages/loggedIn"
+import {LoggedIn} from "./pages/loggedIn"
 import UnloggedHome from "./pages/UnloggedHome";
 import React from "react";
+import {CreateRoom} from "./pages/createroom";
 export const Rts = () =>{
     return(
         <Router>
             <Routes>
                 <Route exact path="/" element={<UnloggedHome />}/>
                 <Route path="/login" element={<Login />}/>
+                <Route path="/createroom" element={<CreateRoom/>}/>
                 <Route path="/loggedIn" element={<LoggedIn />}/>
             </Routes>
         </Router>
