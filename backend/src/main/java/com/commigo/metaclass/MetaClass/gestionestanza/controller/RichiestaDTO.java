8f9eb61a6400eb136e68dc3ca597505e18e357b1
Alberti Salvatore <alberti_salvatore@outlook.it>
2024-01-03 14:29:30 +0100

    fix(backend):
    now promuoviOrganizzatore work

---
+++ backend/src/main/java/com/commigo/metaclass/MetaClass/gestionestanza/controller/RichiestaDTO.java
@@ -0,0 +1,13 @@
+package com.commigo.metaclass.MetaClass.gestionestanza.controller;
+public class RichiestaDTO {
+    private long id_og;
+    private long id_stanza;
+
+    public long getId_og() {
+        return id_og;
+    }
+
+    public long getId_stanza() {
+        return id_stanza;
+    }
+}
