950f925572da7c888636e592e996fece9c8cadd1
Michele <m.pesce13@studenti.unisa.it>
2024-01-04 14:36:26 +0100

    feat(backend):
    add visualizzaDatiUtente

--- backend/src/main/java/com/commigo/metaclass/MetaClass/gestioneutenza/service/GestioneUtenzaService.java
+++ backend/src/main/java/com/commigo/metaclass/MetaClass/gestioneutenza/service/GestioneUtenzaService.java
@@ -3,6 +3,7 @@ package com.commigo.metaclass.MetaClass.gestioneutenza.service;
 import com.commigo.metaclass.MetaClass.entity.Stanza;
 import com.commigo.metaclass.MetaClass.entity.Utente;
 import com.commigo.metaclass.MetaClass.gestioneutenza.controller.ResponseBoolMessage;
+import com.commigo.metaclass.MetaClass.gestioneutenza.exception.DataNotFoundException;
 import com.commigo.metaclass.MetaClass.utility.response.Response;
 
 import java.util.List;
@@ -11,7 +12,8 @@ import java.util.Map;
 
 public interface GestioneUtenzaService{
     boolean loginMeta(Utente u);
-    Response<Boolean> modificaDatiUtente(Long Id, Map<String, Object> dataMap, Utente u);
+    Response<Boolean> modificaDatiUtente(String sessionID, Utente u);
     List<Stanza> getStanzeByUserId(String MetaId);
+    Utente getUtenteByUserId(String sessionID) throws DataNotFoundException;
 
 }
