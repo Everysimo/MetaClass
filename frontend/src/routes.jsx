5eb08523e11e97288280c38aea508f3e73c02e5f
carmi <c.detta@studenti.unisa.it>
2024-01-04 15:45:06 +0100

    feat(frontend):
    routes aggiornamento

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {Login} from "./pages/login";
+import {LoggedIn} from "./pages/loggedIn"
 import UnloggedHome from "./pages/UnloggedHome";
 import {CreateRoom} from "./pages/createroom";
-import {Account} from "./pages/Account";
+import {UserProfile} from "./pages/userprofile";
+import {ModifyRoom} from "./pages/modifyroom";
+
+
 
 export const Rts = () =>{
     return(
@@ -12,7 +16,9 @@ export const Rts = () =>{
                 <Route exact path="/" element={<UnloggedHome />}/>
                 <Route path="/login" element={<Login />}/>
                 <Route path="/createroom" element={<CreateRoom/>}/>
-                <Route path="/Account" element={<Account />}/>
+                <Route path="/modifyroom" element={<ModifyRoom/>}/>
+                <Route path="/loggedIn" element={<LoggedIn />}/>
+                <Route path="/userprofile" element={<UserProfile/>}/>
             </Routes>
         </Router>
     )
