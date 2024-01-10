160d80731c9a4927eba35e5b20d2c3b4d7f32a65
carmi <c.detta@studenti.unisa.it>
2024-01-10 10:17:19 +0100

    feat(frontend):
    push per modifica nome partecipante, silenzia e kick

--- frontend/src/pages/SingleRoom.jsx
+++ frontend/src/pages/SingleRoom.jsx
@@ -1,28 +1,22 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {MyHeader} from "../components/Header/Header";
 import {MyFooter} from "../components/Footer/Footer";
 import {useNavigate, useParams} from "react-router-dom";
 import CalendarComp from "../components/Calendar/CalendarComp";
-import {checkRole} from "../functions/checkRole";
+
 
 export const SingleRoom = () =>{
     const navigate = useNavigate();
-    const { id: id_stanza } = useParams();
-    const [role, setRole] = useState(false); // Default role value
-    sessionStorage.setItem('idStanza', id_stanza);
-    useEffect(() => {
-        const fetchRole = async () => {
-            try {
-                const fetchedRole = await checkRole(id_stanza);
-                setRole(fetchedRole === 'Organizzatore' || 'Organizzatore_Master'); // Update role state
-            } catch (error) {
-                console.error(error);
-                // Handle error fetching role, set role state accordingly if needed
-            }
-        };
+    const { id: id_stanza } = useParams();      //si usa useParams per farsi passare il parametro
 
-        fetchRole(); // Fetch the role when the component mounts
-    }, [id_stanza]); // Run effect whenever id_stanza changes
+    sessionStorage.setItem('idStanza', id_stanza);
+
+
+    const handleGoToUserList= () => {
+        // Naviga alla pagina di destinazione con il valore 42
+        navigate(`/userListRoom/ ${id_stanza}`);
+    };
+
     const handleGoToModifyDataRoom= () => {
         // Naviga alla pagina di destinazione con il valore 42
         navigate(`/modifyroom/ ${id_stanza}`);
@@ -49,13 +43,12 @@ export const SingleRoom = () =>{
             <section className={"contentSec"}>
                 <h1>Stanza {id_stanza}</h1>
                 <div className={"masterDiv"}>
-                    {role &&
-                        <div className={"childDiv"}>
-                            <h2>Schedula un nuovo meeting</h2>
-                            <CalendarComp/>
-                        </div>
-                    }
                     <div className={"childDiv"}>
+                        <h2>Schedula un nuovo meeting</h2>
+                        <CalendarComp/>
+                    </div>
+                    <div className={"childDiv"}>
+                        <button onClick={handleGoToUserList}>Visualizza Lista Utenti in Stanza</button>
                         <button onClick={handleGoToModifyDataRoom}>Modifica la stanza</button>
                         <button onClick={handleGoToChangeScenario}>Modifica lo scenario della stanza</button>
                         <button onClick={handleGoToAccessManagement}>Gestione degli accessi</button>
