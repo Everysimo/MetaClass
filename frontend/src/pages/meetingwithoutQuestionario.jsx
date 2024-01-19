d8ff84f6c758e91b2fcca82828f236c1ab72129d
carmi <c.detta@studenti.unisa.it>
2024-01-19 02:32:57 +0100

    fix(frontend):
    compila questionario

--- frontend/src/pages/meetingwithoutQuestionario.jsx
+++ frontend/src/pages/meetingwithoutQuestionario.jsx
@@ -1,10 +1,11 @@
 import {MyHeader} from "../components/Layout/Header/Header";
 import {MyFooter} from "../components/Layout/Footer/Footer";
 import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 
 export const MeetingXQuestionario = () => {
 
-    const {array, setArray} = useState([]);
+    const [arrayMeeting, setArray] = useState([]);
 
 
     useEffect(() => {
@@ -28,8 +29,16 @@ export const MeetingXQuestionario = () => {
             }
 
             const data = await response.json();
-            console.log("array dei meeting", data);
+            //console.log("array dei meeting", data);
+            const meetings = data.value || [];
+            setArray(meetings);
 
+/*
+            arrayMeeting.forEach((parametro, indice) => {
+                const nome = parametro.nome;
+                console.log(`Nome: ${indice + 1}: ${nome}`);
+            });
+*/
 
         } catch (error) {
             console.error('Errore durante il recupero deim:', error.message);
@@ -42,12 +51,31 @@ export const MeetingXQuestionario = () => {
                 <MyHeader/>
             </header>
 
-            form
+            <h3>MEETING CON QUESTIONARIO DA COMPILARE</h3>
+
+            <div className={"table-container"} style={{background: "transparent"}}>
+                {arrayMeeting.map((meeting) => (
+                    <div key={meeting.id} className={"table-row"}>
+                        <span className={"table-cell"}><h3>Nome del Meeting: {meeting.nome}</h3></span>
+                        <span className={"table-cell"}><p>Inizio: {meeting.inizio}</p></span>
+                        <span className={"table-cell"}><p>Fine: {meeting.fine}</p></span>
+                        <span className={"table-cell"}><p>ID della Stanza: {meeting.id_stanza.id}</p></span>
+
+                        <button>
+                            <Link to={`/Questionario/${meeting.id}`}
+                                  style={{color: 'inherit', textDecoration: 'none'}}>
+                                Compila Questionario
+                            </Link>
+                        </button>
+
+                    </div>
+                ))}
+            </div>
+
 
             <footer>
                 <MyFooter/>
             </footer>
         </>
     );
-
 }
