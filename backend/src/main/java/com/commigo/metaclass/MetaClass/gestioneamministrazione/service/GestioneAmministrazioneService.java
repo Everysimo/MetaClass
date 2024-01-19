b33dd54fc2032d3e586443e93d213d84f0155515
Michele <m.pesce13@studenti.unisa.it>
2024-01-19 02:30:31 +0100

    fix(backend):
    fix compilaquestionario

--- backend/src/main/java/com/commigo/metaclass/MetaClass/gestioneamministrazione/service/GestioneAmministrazioneService.java
+++ backend/src/main/java/com/commigo/metaclass/MetaClass/gestioneamministrazione/service/GestioneAmministrazioneService.java
@@ -4,13 +4,14 @@ import com.commigo.metaclass.MetaClass.entity.Categoria;
 import com.commigo.metaclass.MetaClass.entity.Scenario;
 import com.commigo.metaclass.MetaClass.entity.Stanza;
 import com.commigo.metaclass.MetaClass.exceptions.RuntimeException403;
+import com.commigo.metaclass.MetaClass.exceptions.ServerRuntimeException;
 
 import java.util.List;
 
 public interface GestioneAmministrazioneService {
 
     boolean updateCategoria(Categoria c);
-    boolean updateScenario(Scenario s, long IdCategoria);
+    boolean updateScenario(Scenario s, long IdCategoria) throws ServerRuntimeException;
     List<Stanza> getStanze();
     boolean deleteBanToUser(Long idUtente, Long idStanza) throws RuntimeException403;
 
